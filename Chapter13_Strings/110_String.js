a64dbc8ada23721a5a2ce8d1d6565c3993bbc4d3
Cicd <project_507464_bot_25d72933a384105a6d98c719715f9478@noreply.gitlab.tech.orange>
2026-08-28 19:02:45 +0530

    StringsBasics

---
+++ Chapter13_Strings/110_String.js
@@ -0,0 +1,16 @@
+// String Properties & Basic Access
+
+let a = "dasdadas\nadasd";
+let a2 = 'dasdad "a"sdsa\ndasd';
+console.log(a);
+console.log(a2);
+let str = "Hello, World!";
+console.log(str.length); // start 1
+console.log(str[0]); // index = 0
+console.log(str[7]);
+console.log(str.at(-1));
+console.log(str.at(-6));
+
+// charAt()
+str.charAt(0); 
+str.charCodeAt(0);  // 72
