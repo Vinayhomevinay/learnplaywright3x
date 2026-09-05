7ae274c7b3e208a04c4e50d03f32ecca52b89da6
Cicd <project_507464_bot_25d72933a384105a6d98c719715f9478@noreply.gitlab.tech.orange>
2026-09-06 02:30:01 +0530

    classes

---
+++ Chapter20_Classes_Objects_OOPS/01_Class_Object/158.js
@@ -0,0 +1,22 @@
+class Person {
+    // Attribute
+    #name;
+    #age;
+
+    // Behaviour
+    eat(){}
+    sleep(){}
+
+}
+
+// CAB -> Class has the A, B
+
+const pramod = new Person();
+// Object - new Person();
+// = Operator
+// pramod -> Object Refence
+
+
+
+const amit = new Person();
+    
