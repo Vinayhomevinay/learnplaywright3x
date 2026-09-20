db97c9806f25ce96bf93919afaa8bc569297dc91
Cicd <project_507464_bot_25d72933a384105a6d98c719715f9478@noreply.gitlab.tech.orange>
2026-09-20 12:43:31 +0530

    3rd assignment

---
+++ Test_Assignment/3.js
@@ -0,0 +1,22 @@
+function classifyJsBasicsScore(score){
+
+if (score>=90){
+    return "EXCELLENT";
+}
+else if (score>=75&&score<=89)
+{
+    return "GOOD";
+}
+else if (score>=50&&score<=74)
+{
+    return "NEEDS_PRACTICE";
+}
+else if (score<50)
+{
+    return "REVISIT";
+}
+
+}
+
+
+classifyJsBasicsScore(95)
