6acd01f8f103fb45055893cba568168e8616c7f6
Cicd <project_507464_bot_25d72933a384105a6d98c719715f9478@noreply.gitlab.tech.orange>
2026-09-20 12:00:51 +0530

    test

---
+++ Test_Assignment/1.js
@@ -0,0 +1,33 @@
+function normalizeJsBasicsLabel(label)
+{
+    let valueafter;
+    valueafter=label.trim();
+    valueafter=valueafter.toLowerCase();    
+
+    const hasSpecial = /[^a-zA-Z0-9]/.test(valueafter);
+    if (hasSpecial){
+         valueafter = valueafter.replace(/[^a-zA-Z0-9]/g, "-");    
+    }
+
+    if (valueafter.length>0)
+    {
+    valueafter="js-basic"+"-"+valueafter;
+    }
+    else
+    {
+      valueafter="js-basic"+valueafter;  
+    }   
+
+    const final = valueafter
+    .replace(/-+/g, "-")        // collapse repeated hyphens into one
+.replace(/^-|-$/g, "");     // remove a hyphen at the start or end
+        console.log(final);
+
+return final; 
+}
+
+ normalizeJsBasicsLabel(" Login Button ")
+ normalizeJsBasicsLabel(" ")
+normalizeJsBasicsLabel("User Profile: Edit!")
+normalizeJsBasicsLabel("---Search@@Box---")
+
